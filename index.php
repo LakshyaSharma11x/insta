e4ee546f5688609da72298c6184ec6d1b74c5b3a
Lakshya Sharma <lakshyasharma11052002@gmail.com>
2025-01-24 12:44:43 +0530

    make file structure

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/root.css">
+    <link rel="stylesheet" href="css/welcome.css">
+</head>
+<body>
+    <div class="wel">
+
+        <div class="wel-inner">
+
+            
+            <div class="wel-left">
+                <div class="l-block">
+                    <div class="cam-name">
+                        INSTAGRAM
+                    </div>
+                    <div class="cam-des">
+                        connecting people
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="wel-right">
+                <div class="form-txt">Login</div>
+                <form class="form" action="" method="">
+                    <div class="form-in-a">
+                        <input type="text" placeholder="Email" class="form-in-b"> 
+                    </div>  
+                    <div class="form-in-a">
+                        <input type="text" placeholder="Password" class="form-in-b"> 
+                    </div>  
+                    <div class="form-in-butx">
+                        <input type="submit" value="login" class="form-in-but"> 
+                    </div>  
+                </form>
+
+                <a href="#" class="form-a">Create new account</a>
+            </div>
+        </div>
+        <div class="line"></div>
+        <!-- <hr> -->
+        <div class="wel-footer">
+            <div class="contributor">
+                Powered by LAKSHYA SHARMA
+            </div>
+        </div>
+    </div>
+</body>
+</html>
